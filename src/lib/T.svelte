e26a8ee7e5fb940320b54274c9baac04939f72dd
Eric Vera <6858503+ericvera@users.noreply.github.com>
2023-11-26 22:16:43 -0400

    Remove TODO that has been fixed

--- src/lib/T.svelte
+++ src/lib/T.svelte
@@ -6,9 +6,6 @@
 </script>
 
 <script lang="ts" generics="TKey extends string">
-	// TODO: Remove once this issue is fixed
-	// https://github.com/sveltejs/svelte-eslint-parser/issues/306
-	/* eslint-disable-next-line no-undef */
 	const { key, t } = $props<Props<TKey>>()
 
 	let text: string
